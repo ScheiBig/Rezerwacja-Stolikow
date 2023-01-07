78c4c381dabf12a457fe481cc1694175ce70f786
Marcin Jeznach <Scheissig@gmail.com>
2023-01-07 15:22:34 +0100

    📟 fix whitespaces

--- backend/src/test/kotlin/com/rezerwacja_stolikow/ApplicationTest.kt
+++ backend/src/test/kotlin/com/rezerwacja_stolikow/ApplicationTest.kt
@@ -3,9 +3,9 @@ package com.rezerwacja_stolikow
 import io.ktor.http.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import kotlin.test.*
 import io.ktor.server.testing.*
-import com.rezerwacja_stolikow.routing.configureRouting
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.*
 
 class ApplicationTest {
     @Test
