240a73b2b4797e1965f1cfd08d6863cda482f1fc
ScheiBig <scheissig@gmail.com>
2023-01-19 13:30:06 +0100

    📱 restructure project files, (re)move unnecessary

---
+++ frontend/src/App.tsx
@@ -0,0 +1,7 @@
+
+export default function App(): React.ReactElement {
+
+  return <>
+  </>
+
+}
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,3 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+// import reportWebVitals from './reportWebVitals'
 import 'tailwindcss/tailwind.css'
 import { BrowserRouter } from "react-router-dom"
 
@@ -15,4 +19,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals()
