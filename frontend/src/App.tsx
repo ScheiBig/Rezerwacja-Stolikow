ad9e2c3148f96732dbe4f8f1b9c453e708fb50f9
ScheiBig <scheissig@gmail.com>
2023-01-19 15:23:26 +0100

    📱 add homepage, temporary navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router';
+import Home from './Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -8,8 +9,15 @@ export default function App(): React.ReactElement {
     {/* <Header expanded/> */}
     <Header />
     <Routes>
+      <Route path={routing.home} element={<Home />} />
     </Routes>
     <Footer />
   </>
 
+}
+
+export const routing = {
+  home: "/",
+  create_reservation: "/create-reservation",
+  list_reservations: "/list-reservations"
 }
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
+import { routing } from "../App";
 
-type HeaderProps = {
-  expanded?: Boolean
-}
+export default function Header(): React.ReactElement {
 
-export default function Header(props: HeaderProps): React.ReactElement {
+  const location = useLocation()
+  const indexHeader = location.pathname === routing.home
+  const nodes = location.pathname.split("/").slice(1)
 
-  
 
-  return <header className="fixed top-0 w-full py-4 px-4 bg-slate-200 dark:bg-slate-800 text-slate-800 dark:text-slate-200 shadow-md">
+  return <div className="fixed top-0 w-full phone:h-44 h-32 shadow-md">
+    <header className={(indexHeader ? "h-full " : "h-16 phone:h-24 ") + "py-4 px-4 bg-slate-200 dark:bg-slate-800 text-slate-800 dark:text-slate-200"}>
       {/* <h1 className="text-center text-5xl py-2">🍽️ Rezerwacja stolików</h1>
       <p className="text-center">Jedz gdzie chcesz, kiedy masz na to ochotę!</p> */}
-      <h1 className={ props.expanded ? "text-center text-5xl py-2" : "inline-block text-3xl pr-3"}>🍽️ Rezerwacja stolików</h1>
-      <p className={ "opacity-75 " + (props.expanded ? "text-center" : "inline-block align-super phone:align-baseline")}>Jedz gdzie chcesz, kiedy masz na to ochotę!</p>
+      <h1 className={indexHeader ? "text-center text-5xl py-2" : "inline-block text-3xl pr-3"}>🍽️ Rezerwacja stolików</h1>
+      <p className={"opacity-75 " + (indexHeader ? "text-center" : "inline-block align-super phone:align-baseline")}>Jedz gdzie chcesz, kiedy masz na to ochotę!</p>
     </header>
+    {!indexHeader && <nav className="h-16 phone:h-20 bg-slate-300 dark:bg-slate-700 flex flex-row items-center text-2xl px-4 gap-x-4">
+      <Link to={routing.home}>🏠</Link>
+      {nodes.map((node) => <Link to={node}>{node}</Link>)}
+    </nav>}
+  </div>
 }
