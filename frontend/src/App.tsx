ac234ecdd3d5a2e71601ac28f71817299033884f
ScheiBig <scheissig@gmail.com>
2023-01-20 04:49:09 +0100

    📱 add context-provider wrappers to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,23 @@ import { Route, Routes } from 'react-router';
 import Home from './Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ReservationListing from './ReservationListing';
+import { HeaderContextProvider } from './context/HeaderContext';
+import { RestaurantsContextProvider } from './context/RestaurantsContext';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient()
 
 export default function App(): React.ReactElement {
 
-  return <>
+  return <QueryClientProvider client={queryClient}><HeaderContextProvider><RestaurantsContextProvider>
     {/* <Header expanded/> */}
     <Header />
     <Routes>
       <Route path={routing.home} element={<Home />} />
     </Routes>
     <Footer />
-  </>
+  </RestaurantsContextProvider></HeaderContextProvider></QueryClientProvider>
 
 }
 
@@ -21,3 +27,5 @@ export const routing = {
   create_reservation: "/create-reservation",
   list_reservations: "/list-reservations"
 }
+
+export const baseUrl = "http://127.0.0.1:42069"
